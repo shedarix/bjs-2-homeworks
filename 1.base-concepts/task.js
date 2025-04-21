3005880839d8822deaea8a3e420fca21649c9aac
Daria <dariaxsh@yandex.ru>
2025-04-21 13:57:35 +0200

    Выполнение задач

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+    const discriminant = b ** 2 - 4 * a * c;
+    if (discriminant < 0) {
+        return [];
+    }
+    if (discriminant === 0) {
+        return [-b / (2 * a)];
+    }
+    const squareRootOfDiscriminant = Math.sqrt(discriminant);
+    return [
+        (-b + squareRootOfDiscriminant) / (2 * a),
+        (-b - squareRootOfDiscriminant) / (2 * a)
+    ];
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+    percent /= 100;
+    const monthlyPercentage = percent / 12;
+    const loanPrincipal = amount - contribution;
+    if (loanPrincipal <= 0 || countMonths <= 0) {
+        return 0;
+    }
+    const paymentFactor = monthlyPercentage + monthlyPercentage / (Math.pow(1 + monthlyPercentage, countMonths) - 1);
+    const monthlyPayment = loanPrincipal * paymentFactor;
+    const totalPayment = monthlyPayment * countMonths;
+  return Number(totalPayment.toFixed(2));
 }
